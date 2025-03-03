d3203793ebcbdd52ecb2fa60dcb4cb76da8592fb
Julia <elnikova_julia@mail.ru>
2025-03-03 21:14:18 +0700

    стилизует страницу 404

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
 import { RoutePath } from '../../const';
+import './not-found-page-style.css';
 
 export default function NotFoundPage(): JSX.Element {
   return (
-    <div style={{ display: 'block', margin: 'auto', width: 'max-content', textAlign: 'center' }}>
+    <div className="not-found-page">
       <h1>404</h1>
       <p>Page not found</p>
       <Link to={RoutePath.Catalog}>В каталог</Link>
     </div>
   );
 }
+
