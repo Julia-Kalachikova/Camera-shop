3792b78351887a0868c250ad093228a2c3b2163d
Julia <kalachikova.j@gmail.com>
2025-05-21 17:46:59 +0700

    доделан попап на удаление товара

--- src/components/cart-list/cart-list.tsx
+++ src/components/cart-list/cart-list.tsx
@@ -1,16 +1,30 @@
+import { useState } from 'react';
 import { selectCartItems } from '../../store/selectors/selectors';
 import { decreaseCount, increaseCount, removeFromCart, setCount } from '../../store/slice/cart-slice';
 import { useAppDispatch, useAppSelector } from '../../store/store-hooks';
+import ModalDeleteItem from '../modal-delete-item/modal-delete-item';
+import { ProductCardType } from '../../types';
 
 
 export default function CartList(): JSX.Element {
 
+  const [itemToDelete, setItemToDelete] = useState<ProductCardType | null>(null);
+
   const cartItems = useAppSelector(selectCartItems);
   const dispatch = useAppDispatch();
 
+  const handleOpenModal = (item: ProductCardType) => {
+    setItemToDelete(item);
+  };
+  const handleCloseModal = () => {
+    setItemToDelete(null);
+  };
+
   const handleRemoveFromCart = (id: number) => {
     dispatch(removeFromCart(id));
+    handleCloseModal();
   };
+
   const handleIncrease = (id: number) => {
     dispatch(increaseCount(id));
   };
@@ -25,85 +39,94 @@ export default function CartList(): JSX.Element {
   };
 
   return (
-    <ul className="basket__list">
-      {cartItems.map((item) => (
-        <li className="basket-item" key={item.id}>
-          <div className="basket-item__img">
-            <picture>
-              <source
-                type={item.type}
-                srcSet={`${item.previewImgWebp}, ${item.previewImgWebp2x}`}
+    <>
+      <ul className="basket__list">
+        {cartItems.map((item) => (
+          <li className="basket-item" key={item.id}>
+            <div className="basket-item__img">
+              <picture>
+                <source
+                  type={item.type}
+                  srcSet={`${item.previewImgWebp}, ${item.previewImgWebp2x}`}
+                />
+                <img
+                  src={item.previewImg}
+                  srcSet={item.previewImg2x}
+                  width={140}
+                  height={120}
+                  alt="{Фотоаппарат} «Орлёнок»"
+                />
+              </picture>
+            </div>
+            <div className="basket-item__description">
+              <p className="basket-item__title">{item.category} «{item.name}»</p>
+              <ul className="basket-item__list">
+                <li className="basket-item__list-item"><span className="basket-item__article">Артикул:</span> <span className="basket-item__number">{item.vendorCode}</span>
+                </li>
+                <li className="basket-item__list-item">{item.type} {item.category}</li>
+                <li className="basket-item__list-item">{item.level} уровень</li>
+              </ul>
+            </div>
+            <p className="basket-item__price"><span className="visually-hidden">Цена:</span>{item.price.toLocaleString('ru-RU')} ₽</p>
+            <div className="quantity">
+              <button
+                className="btn-icon btn-icon--prev"
+                aria-label="уменьшить количество товара"
+                onClick={() => handleDecrease(item.id)}
+                disabled={item.count <= 1}
+              >
+                <svg width="7" height="12" aria-hidden="true">
+                  <use xlinkHref="#icon-arrow"></use>
+                </svg>
+              </button>
+              <label
+                className="visually-hidden"
+                htmlFor="counter1"
+              >
+              </label>
+              <input
+                type="number"
+                id={`counter-${item.id}`}
+                value={item.count}
+                min="1"
+                max="99"
+                onChange={(e) => handleSetCount(item.id, e.target.value)}
+                aria-label="количество товара"
               />
-              <img
-                src={item.previewImg}
-                srcSet={item.previewImg2x}
-                width={140}
-                height={120}
-                alt="{Фотоаппарат} «Орлёнок»"
-              />
-            </picture>
-          </div>
-          <div className="basket-item__description">
-            <p className="basket-item__title">{item.category} «{item.name}»</p>
-            <ul className="basket-item__list">
-              <li className="basket-item__list-item"><span className="basket-item__article">Артикул:</span> <span className="basket-item__number">{item.vendorCode}</span>
-              </li>
-              <li className="basket-item__list-item">{item.type} {item.category}</li>
-              <li className="basket-item__list-item">{item.level} уровень</li>
-            </ul>
-          </div>
-          <p className="basket-item__price"><span className="visually-hidden">Цена:</span>{item.price.toLocaleString('ru-RU')} ₽</p>
-          <div className="quantity">
+              <button
+                className="btn-icon btn-icon--next"
+                aria-label="увеличить количество товара"
+                onClick={() => handleIncrease(item.id)}
+                disabled={item.count >= 99}
+              >
+                <svg width="7" height="12" aria-hidden="true">
+                  <use xlinkHref="#icon-arrow"></use>
+                </svg>
+              </button>
+            </div>
+            <div className="basket-item__total-price">
+              <span className="visually-hidden">Общая цена:</span>{(item.price * item.count).toLocaleString('ru-RU')} ₽
+            </div>
             <button
-              className="btn-icon btn-icon--prev"
-              aria-label="уменьшить количество товара"
-              onClick={() => handleDecrease(item.id)}
-              disabled={item.count <= 1}
+              className="cross-btn"
+              type="button"
+              aria-label="Удалить товар"
+              onClick={() => handleOpenModal(item)}
             >
-              <svg width="7" height="12" aria-hidden="true">
-                <use xlinkHref="#icon-arrow"></use>
+              <svg width="10" height="10" aria-hidden="true">
+                <use xlinkHref="#icon-close"></use>
               </svg>
             </button>
-            <label
-              className="visually-hidden"
-              htmlFor="counter1"
-            >
-            </label>
-            <input
-              type="number"
-              id={`counter-${item.id}`}
-              value={item.count}
-              min="1"
-              max="99"
-              onChange={(e) => handleSetCount(item.id, e.target.value)}
-              aria-label="количество товара"
-            />
-            <button
-              className="btn-icon btn-icon--next"
-              aria-label="увеличить количество товара"
-              onClick={() => handleIncrease(item.id)}
-              disabled={item.count >= 99}
-            >
-              <svg width="7" height="12" aria-hidden="true">
-                <use xlinkHref="#icon-arrow"></use>
-              </svg>
-            </button>
-          </div>
-          <div className="basket-item__total-price">
-            <span className="visually-hidden">Общая цена:</span>{(item.price * item.count).toLocaleString('ru-RU')} ₽
-          </div>
-          <button
-            className="cross-btn"
-            type="button"
-            aria-label="Удалить товар"
-            onClick={() => handleRemoveFromCart(item.id)}
-          >
-            <svg width="10" height="10" aria-hidden="true">
-              <use xlinkHref="#icon-close"></use>
-            </svg>
-          </button>
-        </li>
-      ))}
-    </ul>
+          </li>
+        ))}
+      </ul>
+      {itemToDelete && (
+        <ModalDeleteItem
+          productCard={itemToDelete}
+          onClose={handleCloseModal}
+          onHandleRemoveFromCart={() => handleRemoveFromCart(itemToDelete.id)}
+        />
+      )}
+    </>
   );
 }
---
+++ src/components/modal-delete-item/modal-delete-item.tsx
@@ -0,0 +1,62 @@
+import { Link } from 'react-router-dom';
+import { ProductCardType } from '../../types';
+import { RoutePath } from '../../const';
+
+type Props = {
+  productCard: ProductCardType;
+  onClose: () => void;
+  onHandleRemoveFromCart: (id: number) => void;
+}
+
+
+export default function ModalDeleteItem({productCard, onClose, onHandleRemoveFromCart}: Props): JSX.Element {
+  return (
+    <div className="modal is-active">
+      <div className="modal__wrapper">
+        <div className="modal__overlay"></div>
+        <div className="modal__content">
+          <p className="title title--h4">Удалить этот товар?</p>
+          <div className="basket-item basket-item--short">
+            <div className="basket-item__img">
+              <picture>
+                <source type="image/webp" srcSet={`${productCard.previewImgWebp}, ${productCard.previewImgWebp2x}`} />
+                <img src={productCard.previewImg} srcSet={productCard.previewImg2x} width="140" height="120" alt={`${productCard.category}, ${productCard.name}`} />
+              </picture>
+            </div>
+            <div className="basket-item__description">
+              <p className="basket-item__title">{productCard.name}</p>
+              <ul className="basket-item__list">
+                <li className="basket-item__list-item"><span className="basket-item__article">Артикул:</span> <span className="basket-item__number">{productCard.vendorCode}</span>
+                </li>
+                <li className="basket-item__list-item">{productCard.type} {productCard.category}</li>
+                <li className="basket-item__list-item">{productCard.level} уровень</li>
+              </ul>
+            </div>
+          </div>
+          <div className="modal__buttons">
+            <button
+              className="btn btn--purple modal__btn modal__btn--half-width"
+              type="button"
+              onClick={() => onHandleRemoveFromCart(productCard.id)}
+            >Удалить
+            </button>
+            <Link
+              className="btn btn--transparent modal__btn modal__btn--half-width" to={RoutePath.Catalog}
+            >Продолжить покупки
+            </Link>
+          </div>
+          <button
+            className="cross-btn"
+            type="button"
+            aria-label="Закрыть попап"
+            onClick={onClose}
+          >
+            <svg width="10" height="10" aria-hidden="true">
+              <use xlinkHref="#icon-close"></use>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
